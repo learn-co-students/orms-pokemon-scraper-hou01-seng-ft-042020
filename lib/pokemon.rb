05bb578018948df1ef92c964b2c93b68848dc77e
estherkimyunjung <estherkimyunjung@gmail.com>
2020-04-30 15:29:09 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,21 +3,14 @@ require 'sqlite3'
 #open a database
 db = SQLite3::Database.new "schema_migration.sql" 
 
-
 class Pokemon
     attr_accessor :id, :name, :type, :db
-    @@all = []
 
-    def initialize(id: id,name: name, type: type, db: db)
+    def initialize(id: id, name: name, type: type, db: db)
         @id = id
         @name = name
         @type = type
         @db = db
-        @@all << self
-    end
-
-    def self.all
-        @@all
     end
 
     def self.save(name, type, db)
@@ -25,8 +18,7 @@ class Pokemon
     end
 
     def self.find(id, db) 
-        new_array = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-        Pokemon.new(id: new_array[0], name: new_array[1], type: new_array[2], db: db)
+        new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+        Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], db: db)
    end
-
 end
